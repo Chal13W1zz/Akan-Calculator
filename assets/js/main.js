f3170766649964ab8dd7c382a8c325ceba254f17
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-09-06 00:52:18 +0300

    add day of birth calculation formula

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 document.getElementById('generate').onclick = function generate(){
     var name = document.getElementById("name").value;
     var yob = document.getElementById("year").options[document.getElementById("year").selectedIndex].text;
-    var mob = document.getElementById("month").options[document.getElementById("month").selectedIndex].text;;
-    var dob = document.getElementById("date").value;
+    var mob = Number(document.getElementById("month").options[document.getElementById("month").selectedIndex].value);
+    var dob = Number(document.getElementById("date").value);
     var gender = document.querySelector('input[name="gender"]:checked').value;
 
     var M = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
@@ -17,6 +17,18 @@ document.getElementById('generate').onclick = function generate(){
         6:"Friday",
         7:"Saturday"
     }
+
+    var century = yob.split("",2).join("");
+
+    var CC = Number(century);
+    var YY = yob;
+    var MM = mob;
+    var DD = dob;
+    
+    var dayOfBirth =  Math.round(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7) ;
+    alert(dayOfBirth);
+
+    
 
     
 }
