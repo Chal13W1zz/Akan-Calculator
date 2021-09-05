d1bc33d59573081be9a0162337524270ba251d6d
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-09-06 00:19:02 +0300

    create two gendar names arrays and a days of the week object

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,5 +5,18 @@ document.getElementById('generate').onclick = function generate(){
     var dob = document.getElementById("date").value;
     var gender = document.querySelector('input[name="gender"]:checked').value;
 
+    var M = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var F = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+
+    const dow = {
+        1:"Sunday",
+        2:"Monday",
+        3:"Tuesday",
+        4:"Wednesday",
+        5:"Thursday",
+        6:"Friday",
+        7:"Saturday"
+    }
+
     
 }
