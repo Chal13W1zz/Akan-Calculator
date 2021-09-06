d293d868e2041af3cea5dc08ae872f5d7c950c14
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-09-06 12:49:45 +0300

    refactor and comment the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,36 @@
+//function call on generate button click
 document.getElementById('generate').onclick = function generate(){
+
+    //store user input in variables
     var name = document.getElementById("name").value;
     var yob = document.getElementById("year").options[document.getElementById("year").selectedIndex].text;
     var mob = Number(document.getElementById("month").options[document.getElementById("month").selectedIndex].value);
     var dob = Number(document.getElementById("date").value);
     var gender = document.querySelector('input[name="gender"]:checked').value;
 
+    //create name arrays and a days array
     var M = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var F = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+    const dow  = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    const dow = {
-        1:"Sunday",
-        2:"Monday",
-        3:"Tuesday",
-        4:"Wednesday",
-        5:"Thursday",
-        6:"Friday",
-        7:"Saturday"
-    }
-
+    //calculate the century using the year of birth
     var century = yob.split("",2).join("");
 
+    //reassign the variables to the formula names
     var CC = Number(century);
     var YY = yob;
     var MM = mob;
     var DD = dob;
-    
-    var dayOfBirth =  Math.round(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7) ;
+
+    //calculate the day of birth
+    var dayOfBirth =  Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7) ;
     alert(dayOfBirth);
 
-    
-
+    //give back an akan name based on the gender
+    if (gender === "female"){
+    alert("You were born on " + dow[dayOfBirth]+ " and your akan name is " + F[dayOfBirth] );
+    } else if(gender === "male"){
+        alert("You were born on " + dow[dayOfBirth]+ " and your akan name is " + M[dayOfBirth] );
+    }
     
 }
