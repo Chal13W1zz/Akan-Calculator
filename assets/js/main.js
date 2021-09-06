1187bfe769c014c037653d28baeff9f24d4536d0
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-09-06 13:52:58 +0300

    add form validation snippets

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,25 @@ document.getElementById('generate').onclick = function generate(){
     var mob = Number(document.getElementById("month").options[document.getElementById("month").selectedIndex].value);
     var dob = Number(document.getElementById("date").value);
     var gender = document.querySelector('input[name="gender"]:checked').value;
+    console.log(dob);
+
+    //Name validation
+    var allletters = /^[A-Za-z]+$/ ;
+    if(name.match(allletters)){
+        //Year of birth validation
+        if (yob.length == 4){
+            //Month of birth validation
+            if(mob <= 12 && mob > 0 ){
+                //date of birth validation
+                if (dob <= 31 && dob > 0 ){
+                    //To Do
+                }else("Date of birth should be between 0 and 31");
+
+            }else( alert("The month should be between january(1) to december(12)"));
+
+        }else( alert("Your year of birth should have 4digits"));
+
+    }else(alert("Your name should not contain whitespaces,numbers or other special characters"));
 
     //create name arrays and a days array
     var M = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
@@ -24,7 +43,6 @@ document.getElementById('generate').onclick = function generate(){
 
     //calculate the day of birth
     var dayOfBirth =  Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7) ;
-    alert(dayOfBirth);
 
     //give back an akan name based on the gender
     if (gender === "female"){
