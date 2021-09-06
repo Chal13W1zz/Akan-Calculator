ee7c7bfc61856a9bd1f63fab34c55792cca6dd6e
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-09-06 15:09:15 +0300

    code refactoring and cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,78 @@
 //function call on generate button click
-document.getElementById('generate').onclick = function generate(){
+document.getElementById('generate').onclick = function generate(e) {
+    e.preventDefault();
+
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function () {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    })()
 
     //store user input in variables
-    var name = document.getElementById("name").value;
+    var name = document.getElementById("fname").value;
     var yob = document.getElementById("year").options[document.getElementById("year").selectedIndex].text;
-    var mob = Number(document.getElementById("month").options[document.getElementById("month").selectedIndex].value);
-    var dob = Number(document.getElementById("date").value);
+    var mob = parseInt(document.getElementById("month").options[document.getElementById("month").selectedIndex].value);
+    var dob = parseInt(document.getElementById("date").options[document.getElementById("date").selectedIndex].value);
     var gender = document.querySelector('input[name="gender"]:checked').value;
-    console.log(dob);
 
     //Name validation
-    var allletters = /^[A-Za-z]+$/ ;
-    if(name.match(allletters)){
+    var allletters = /^[A-Za-z]+$/;
+    if (name.match(allletters)) {
         //Year of birth validation
-        if (yob.length == 4){
+        if (yob.length == 4) {
             //Month of birth validation
-            if(mob <= 12 && mob > 0 ){
+            if (mob <= 12 && mob > 0) {
                 //date of birth validation
-                if (dob <= 31 && dob > 0 ){
-                    //To Do
-                }else("Date of birth should be between 0 and 31");
+                if (dob <= 31 && dob > 0) {
 
-            }else( alert("The month should be between january(1) to december(12)"));
 
-        }else( alert("Your year of birth should have 4digits"));
+                    //create name arrays and a days array
+                    var M = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+                    var F = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+                    const dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    }else(alert("Your name should not contain whitespaces,numbers or other special characters"));
+                    //calculate the century using the year of birth
+                    var century = yob.split("", 2).join("");
 
-    //create name arrays and a days array
-    var M = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var F = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
-    const dow  = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                    //reassign the variables to the formula names
+                    var CC = Number(century);
+                    var YY = yob;
+                    var MM = mob;
+                    var DD = dob;
 
-    //calculate the century using the year of birth
-    var century = yob.split("",2).join("");
+                    //calculate the day of birth
+                    var dayOfBirth = Math.floor((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
 
-    //reassign the variables to the formula names
-    var CC = Number(century);
-    var YY = yob;
-    var MM = mob;
-    var DD = dob;
+                    //give back an akan name based on the gender
+                    if (gender === "female") {
+                        alert("You were born on " + dow[dayOfBirth] + " and your akan name is " + F[dayOfBirth]);
+                    } else if (gender === "male") {
+                        alert("You were born on " + dow[dayOfBirth] + " and your akan name is " + M[dayOfBirth]);
+                    }
 
-    //calculate the day of birth
-    var dayOfBirth =  Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7) ;
+                } else ("Date of birth should be between 0 and 31");
 
-    //give back an akan name based on the gender
-    if (gender === "female"){
-    alert("You were born on " + dow[dayOfBirth]+ " and your akan name is " + F[dayOfBirth] );
-    } else if(gender === "male"){
-        alert("You were born on " + dow[dayOfBirth]+ " and your akan name is " + M[dayOfBirth] );
-    }
-    
+            } else (alert("The month should be between january(1) to december(12)"));
+
+        } else (alert("Your year of birth should have 4digits"));
+
+    } else (alert("Your name should not contain whitespaces,numbers or other special characters"));
+
+
 }
